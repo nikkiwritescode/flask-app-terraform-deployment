8324c12c9a67eceb8965ad968cf9a65d70e873ad
nikkiwritescode <nicole.farris@outlook.com>
2021-06-07 10:51:44 -0500

    Further cleaning of unneeded, leftover comments.

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "candidate-table" { #"flask-db-table-1" {
+resource "aws_dynamodb_table" "candidate-table" {
   name           = "Candidates"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "CandidateName"
