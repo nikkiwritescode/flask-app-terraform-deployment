084a333c3cb1b74f535f43111c8a4d363e92acd8
nikkiwritescode <nicole.farris@outlook.com>
2021-06-06 21:08:49 -0500

    Further updated the configuration to support the CI/CD pipeline, namely allowing values to support being passed in as environment variables.

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -2,6 +2,11 @@ variable "app_name" {}
 variable "dynamo_table_name" {}
 variable "env_prefix" {}
 variable "elb_dns" {}
-variable "my_public_key_location" {}
+variable "my_public_key_location" {
+    default = "~/.ssh/id_rsa.pub"
+}
+variable "my_public_key" {
+    default = "empty"
+}
 variable "my_ip" {}
 variable "vpc_id" {}
--- variables.tf
+++ variables.tf
@@ -7,6 +7,11 @@ variable "asg_min_size" {}
 variable "instance_type" {}
 variable "ami_id" {}
 variable "my_ip" {}
-variable "my_public_key_location" {}
+variable "my_public_key_location" {
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "my_public_key" {
+  default = "empty"
+}
 variable "subnet_cidr_blocks" {}
 variable "vpc_cidr_block" {}
